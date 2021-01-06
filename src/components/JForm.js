6acd4e0a9116cff3bf8b508af7b27b2d90d65780
Jimmy Svensson <jimmy888swe@gmail.com>
2021-01-06 21:29:57 +0100

    form

--- src/components/JForm.js
+++ src/components/JForm.js
@@ -8,27 +8,43 @@ function JForm() {
 
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
-  const [message, setMassage] = useState('');
-  const [disabeld, setDisabel] = useState(false);
+  const [message, setMessage] = useState('');
+  const [disabled, setDisabled] = useState(false);
   const [sent, setSent] = useState(null);
-  
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    let emailToSend ={
+      name,
+      email,
+      message
+    }
+
+    console.log(emailToSend, 'Skickat');
+
+    // reset
+    setName("");
+    setEmail("");
+    setMessage("");
+  }
+
   return (
   <Container>
     <Col  md={{ span: 6, offset: 3 }}>
-      <Form>
-        <Form.Group controlId="exampleForm.ControlInput1">
-          <Form.Label>Namn</Form.Label>
-          <Form.Control type="text" placeholder="Namn" />
+      <Form onSubmit={submitHandler}>
+        <Form.Group>
+          <Form.Label htmlFor="name">Namn</Form.Label>
+          <Form.Control type="text" placeholder="Namn" name="name" id="name" onChange={e => setName(e.target.value)} value={name} />
         </Form.Group>
-        <Form.Group controlId="exampleForm.ControlInput1">
-          <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="name@example.com" />
+        <Form.Group>
+          <Form.Label htmlFor="email">Email</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" name="email" id="email" onChange={e => setEmail(e.target.value)} value={email} />
         </Form.Group>
-        <Form.Group controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Meddelande</Form.Label>
-          <Form.Control as="textarea" rows={3} placeholder="Meddelande..." />
+        <Form.Group>
+          <Form.Label htmlFor="message">Meddelande</Form.Label>
+          <Form.Control as="textarea" rows={3} placeholder="Meddelande..." name="message" id="message" onChange={e => setMessage(e.target.value)} value={message} />
         </Form.Group>
-        <Button variant="secondary" size="lg" active style={{marginBottom:"100px"}}>Skicka</Button>
+        <Button variant="secondary" size="lg" active style={{marginBottom:"100px"}} type="submit" disabled={disabled}>Skicka</Button>
       </Form>
     </Col>
   </Container>
